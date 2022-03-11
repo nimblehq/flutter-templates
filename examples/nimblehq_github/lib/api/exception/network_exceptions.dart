683a33787c92351fe1b1d994b649c5fa608fe986
manh <manh@nimblehq.co>
2022-03-11 14:30:15 +0700

    [#44] Init nimblehq_github project

---
+++ examples/nimblehq_github/lib/api/exception/network_exceptions.dart
@@ -0,0 +1,162 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'network_exceptions.freezed.dart';
+
+@freezed
+class NetworkExceptions with _$NetworkExceptions {
+  const factory NetworkExceptions.requestCancelled() = RequestCancelled;
+
+  const factory NetworkExceptions.unauthorisedRequest() = UnauthorisedRequest;
+
+  const factory NetworkExceptions.badRequest() = BadRequest;
+
+  const factory NetworkExceptions.notFound(String reason) = NotFound;
+
+  const factory NetworkExceptions.methodNotAllowed() = MethodNotAllowed;
+
+  const factory NetworkExceptions.notAcceptable() = NotAcceptable;
+
+  const factory NetworkExceptions.requestTimeout() = RequestTimeout;
+
+  const factory NetworkExceptions.receiveTimeout() = ReceiveTimeout;
+
+  const factory NetworkExceptions.sendTimeout() = SendTimeout;
+
+  const factory NetworkExceptions.conflict() = Conflict;
+
+  const factory NetworkExceptions.internalServerError() = InternalServerError;
+
+  const factory NetworkExceptions.notImplemented() = NotImplemented;
+
+  const factory NetworkExceptions.serviceUnavailable() = ServiceUnavailable;
+
+  const factory NetworkExceptions.noInternetConnection() = NoInternetConnection;
+
+  const factory NetworkExceptions.formatException() = FormatException;
+
+  const factory NetworkExceptions.unableToProcess() = UnableToProcess;
+
+  const factory NetworkExceptions.defaultError(String error) = DefaultError;
+
+  const factory NetworkExceptions.unexpectedError() = UnexpectedError;
+
+  static NetworkExceptions fromDioException(error) {
+    if (error is Exception) {
+      try {
+        NetworkExceptions networkExceptions;
+        if (error is DioError) {
+          switch (error.type) {
+            case DioErrorType.cancel:
+              networkExceptions = NetworkExceptions.requestCancelled();
+              break;
+            case DioErrorType.connectTimeout:
+              networkExceptions = NetworkExceptions.requestTimeout();
+              break;
+            case DioErrorType.other:
+              networkExceptions = NetworkExceptions.noInternetConnection();
+              break;
+            case DioErrorType.receiveTimeout:
+              networkExceptions = NetworkExceptions.receiveTimeout();
+              break;
+            case DioErrorType.sendTimeout:
+              networkExceptions = NetworkExceptions.sendTimeout();
+              break;
+            case DioErrorType.response:
+              switch (error.response?.statusCode) {
+                case 400:
+                  networkExceptions = NetworkExceptions.badRequest();
+                  break;
+                case 401:
+                  networkExceptions = NetworkExceptions.unauthorisedRequest();
+                  break;
+                case 403:
+                  networkExceptions = NetworkExceptions.unauthorisedRequest();
+                  break;
+                case 404:
+                  networkExceptions = NetworkExceptions.notFound("Not found");
+                  break;
+                case 409:
+                  networkExceptions = NetworkExceptions.conflict();
+                  break;
+                case 408:
+                  networkExceptions = NetworkExceptions.requestTimeout();
+                  break;
+                case 500:
+                  networkExceptions = NetworkExceptions.internalServerError();
+                  break;
+                case 503:
+                  networkExceptions = NetworkExceptions.serviceUnavailable();
+                  break;
+                default:
+                  var responseCode = error.response?.statusCode;
+                  networkExceptions = NetworkExceptions.defaultError(
+                    "Received invalid status code: $responseCode",
+                  );
+              }
+              break;
+          }
+        } else if (error is SocketException) {
+          networkExceptions = NetworkExceptions.noInternetConnection();
+        } else {
+          networkExceptions = NetworkExceptions.unexpectedError();
+        }
+        return networkExceptions;
+      } on FormatException catch (_) {
+        return NetworkExceptions.formatException();
+      } catch (_) {
+        return NetworkExceptions.unexpectedError();
+      }
+    } else {
+      if (error.toString().contains("is not a subtype of")) {
+        return NetworkExceptions.unableToProcess();
+      } else {
+        return NetworkExceptions.unexpectedError();
+      }
+    }
+  }
+
+  static String getErrorMessage(NetworkExceptions networkExceptions) {
+    var errorMessage = "";
+    networkExceptions.when(notImplemented: () {
+      errorMessage = "Not Implemented";
+    }, requestCancelled: () {
+      errorMessage = "Request Cancelled";
+    }, internalServerError: () {
+      errorMessage = "Internal Server Error";
+    }, notFound: (String reason) {
+      errorMessage = reason;
+    }, serviceUnavailable: () {
+      errorMessage = "Service unavailable";
+    }, methodNotAllowed: () {
+      errorMessage = "Method not allowed";
+    }, badRequest: () {
+      errorMessage = "Bad request";
+    }, unauthorisedRequest: () {
+      errorMessage = "Unauthorised request";
+    }, unexpectedError: () {
+      errorMessage = "Unexpected error occurred";
+    }, requestTimeout: () {
+      errorMessage = "Connection request timeout";
+    }, noInternetConnection: () {
+      errorMessage = "No internet connection";
+    }, conflict: () {
+      errorMessage = "Error due to a conflict";
+    }, sendTimeout: () {
+      errorMessage = "Send timeout in connection with API server";
+    }, receiveTimeout: () {
+      errorMessage = "Receive timeout in connection with API server";
+    }, unableToProcess: () {
+      errorMessage = "Unable to process the data";
+    }, defaultError: (String error) {
+      errorMessage = error;
+    }, formatException: () {
+      errorMessage = "Unexpected error occurred";
+    }, notAcceptable: () {
+      errorMessage = "Not acceptable";
+    });
+    return errorMessage;
+  }
+}
---
+++ examples/nimblehq_github/lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_templates/gen/assets.gen.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterConfig.loadEnvVariables();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // TODO: implement Routes then remove `home: MyHomePage()` and use initialRoute instead.
+  final String initialRoute;
+
+  MyApp({this.initialRoute = '/'});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        fontFamily: Assets.fonts.neuzeit,
+      ),
+      home: MyHomePage(),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: FutureBuilder<PackageInfo>(
+            future: PackageInfo.fromPlatform(),
+            builder: (context, snapshot) {
+              return snapshot.hasData
+                  ? Text(snapshot.data?.appName ?? "")
+                  : SizedBox.shrink();
+            }),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 24),
+            FractionallySizedBox(
+              widthFactor: 0.5,
+              child: Image.asset(
+                Assets.images.nimbleLogo.path,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            SizedBox(height: 24),
+            Text(AppLocalizations.of(context)!.hello),
+            Text(
+              FlutterConfig.get('SECRET'),
+              style: TextStyle(color: Colors.black, fontSize: 24),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
