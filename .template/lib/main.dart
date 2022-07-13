6f85241ff3b19070135dc45ad843e801da356f0d
Tran Manh <manh@nimblehq.co>
2022-07-13 15:45:00 +0700

    [#96] Add go_router as a navigator

--- .template/lib/main.dart
+++ .template/lib/main.dart
@@ -2,34 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_templates/gen/assets.gen.dart';
+import 'package:go_router/go_router.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterConfig.loadEnvVariables();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // TODO: implement Routes then remove `home: MyHomePage()` and use initialRoute instead.
-  final String initialRoute;
+  MyApp({Key? key}) : super(key: key);
 
-  const MyApp({
-    Key? key,
-    this.initialRoute = '/',
-  }) : super(key: key);
+  final GoRouter _router = GoRouter(
+    routes: <GoRoute>[
+      GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) =>
+            const MyHomePage(),
+        routes: [
+          GoRoute(
+            path: 'page2',
+            builder: (BuildContext context, GoRouterState state) =>
+                const Page2Screen(),
+          ),
+          GoRoute(
+            path: 'page3',
+            builder: (BuildContext context, GoRouterState state) =>
+                const Page3Screen(),
+          ),
+        ],
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       theme: ThemeData(
         primarySwatch: Colors.blue,
         brightness: Brightness.light,
         fontFamily: Assets.fonts.neuzeit,
       ),
-      home: const MyHomePage(),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
+      routeInformationProvider: _router.routeInformationProvider,
+      routeInformationParser: _router.routeInformationParser,
+      routerDelegate: _router.routerDelegate,
     );
   }
 }
@@ -65,10 +84,55 @@ class MyHomePage extends StatelessWidget {
             Text(
               FlutterConfig.get('SECRET'),
               style: const TextStyle(color: Colors.black, fontSize: 24),
-            )
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () => context.go('/page2'),
+              child: const Text("Navigate to Page 2"),
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class Page3Screen extends StatelessWidget {
+  const Page3Screen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Page 3"),
+      ),
+      body: Center(
+          child: ElevatedButton(
+        onPressed: () => context.pop(),
+        child: const Text("Back"),
+      )),
+    );
+  }
+}
+
+class Page2Screen extends StatelessWidget {
+  const Page2Screen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Page 2"),
+      ),
+      body: Center(
+          child: ElevatedButton(
+        onPressed: () => context.push('/page3'),
+        child: const Text("Navigate to Page 3"),
+      )),
     );
   }
 }
