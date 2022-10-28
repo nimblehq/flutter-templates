bafba83e5375cf56062bbf2b115a057d420e6ca7
doan@nimblehq.co <doan@nimblehq.co>
2022-10-28 15:55:33 +0700

    [#101] Add comment for refresh token

--- .template/lib/di/interceptor/app_interceptor.dart
+++ .template/lib/di/interceptor/app_interceptor.dart
@@ -22,7 +22,7 @@ class AppInterceptor extends Interceptor {
   void onError(DioError err, ErrorInterceptorHandler handler) {
     final statusCode = err.response?.statusCode;
     if ((statusCode == HttpStatus.forbidden ||
-        statusCode == HttpStatus.unauthorized) &&
+            statusCode == HttpStatus.unauthorized) &&
         _requireAuthenticate) {
       _doRefreshToken(err, handler);
     } else {
@@ -31,11 +31,34 @@ class AppInterceptor extends Interceptor {
   }
 
   Future<void> _doRefreshToken(
-      DioError err,
-      ErrorInterceptorHandler handler,
-      ) async {
-    //TODO handle refresh token here
-
+    DioError err,
+    ErrorInterceptorHandler handler,
+  ) async {
+    // try {
+    //   // TODO Request new token
+    //
+    //   if (result is Success) {
+    //     // TODO Update new token header
+    //     // err.requestOptions.headers[_headerAuthorization] = newToken;
+    //     // Create request with new access token
+    //     final options = Options(
+    //         method: err.requestOptions.method,
+    //         headers: err.requestOptions.headers);
+    //     final newRequest = await _dio.request(
+    //         "${err.requestOptions.baseUrl}${err.requestOptions.path}",
+    //         options: options,
+    //         data: err.requestOptions.data,
+    //         queryParameters: err.requestOptions.queryParameters);
+    //     handler.resolve(newRequest);
+    //   } else {
+    //     handler.next(err);
+    //   }
+    // } catch (exception) {
+    //   if (exception is DioError) {
+    //     handler.next(exception);
+    //   } else {
+    //     handler.next(err);
+    //   }
+    // }
   }
-
 }
