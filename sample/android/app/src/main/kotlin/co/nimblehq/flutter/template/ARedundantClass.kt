b6359f9c8eec6da9321152eb64b0cd458f6082d4
Luong Vo (Lucas) <luong@nimblehq.co>
2023-05-14 11:32:24 +0700

    [#159] Test

---
+++ sample/android/app/src/main/kotlin/co/nimblehq/flutter/template/ARedundantClass.kt
@@ -0,0 +1,1 @@
+// This sample file should be removed because it does not exist in the new revision.
---
+++ sample/android/app/src/main/kotlin/co/nimblehq/flutter/template/MainActivity.kt
@@ -0,0 +1,7 @@
+package co.nimblehq.flutter.template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // This class should be updated
+}
