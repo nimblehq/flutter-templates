584891df228f52e6361e804597e7336cf1d5658d
Doan Dinh <doan@nimblehq.co>
2023-07-22 17:42:29 +0700

    [#223] Remove unused import

--- bricks/template/__brick__/{{project_name.snakeCase()}}/test/mocks/response/user_response_mocks.dart
+++ bricks/template/__brick__/{{project_name.snakeCase()}}/test/mocks/response/user_response_mocks.dart
@@ -2,7 +2,7 @@ import 'package:{{project_name.snakeCase()}}/api/response/user_response.dart';
 
 class UserResponseMocks {
   static UserResponse mock() {
-    return const UserResponse(
+    return UserResponse(
       email: "email",
       username: "username",
     );
--- bricks/template/__brick__/{{project_name.snakeCase()}}/test/usecases/user/get_users_use_case_test.dart
+++ bricks/template/__brick__/{{project_name.snakeCase()}}/test/usecases/user/get_users_use_case_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:{{project_name.snakeCase()}}/model/user.dart';
 import 'package:{{project_name.snakeCase()}}/usecases/base/base_use_case.dart';
 import 'package:{{project_name.snakeCase()}}/usecases/user/get_users_use_case.dart';
 
