8d0e89c81edaaf8f60457ed64e05feb2208f5813
Doan Dinh <doan@nimblehq.co>
2023-08-02 23:46:37 +0700

    [#258] Update network module path

--- bricks/template/__brick__/{{project_name.snakeCase()}}/lib/di/module/network_module.dart
+++ bricks/template/__brick__/{{project_name.snakeCase()}}/lib/di/module/network_module.dart
@@ -1,4 +1,4 @@
-import 'package:{{project_name.snakeCase()}}/api/api_service.dart';
+import 'package:{{project_name.snakeCase()}}/data/datasources/remote/api_service.dart';
 import 'package:{{project_name.snakeCase()}}/env.dart';
 import 'package:{{project_name.snakeCase()}}/di/provider/dio_provider.dart';
 import 'package:injectable/injectable.dart';
