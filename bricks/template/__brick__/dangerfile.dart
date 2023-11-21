f88a03c6a61b43977d4c176b88b2059a33c030b4
Doan Dinh <doan@nimblehq.co>
2023-11-21 13:56:00 +0700

    [#242] Add lint into dangerfile

--- bricks/template/__brick__/dangerfile.dart
+++ bricks/template/__brick__/dangerfile.dart
@@ -1,7 +1,3 @@
-import 'package:danger_core/danger_core.dart';
-
-void main() {
-  if (danger.github.pr.title.contains('WIP')) {
-    warn('PR is considered WIP');
-  }
-}
+flutter_lint.only_modified_files = true
+flutter_lint.report_path = "flutter_analyze_report.txt"
+flutter_lint.lint(inline_mode: true)
